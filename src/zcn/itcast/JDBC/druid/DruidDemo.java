9c6bfe3c4d9e517b5c6cccc0ebf48f7b41420458
c3730915 <1023700418@qq.com>
2020-10-06 15:16:59 +0800

    更新注释

--- src/zcn/itcast/JDBC/druid/DruidDemo.java
+++ src/zcn/itcast/JDBC/druid/DruidDemo.java
@@ -12,9 +12,10 @@ import java.util.Properties;
 public class DruidDemo {
     public static void main(String[] args) throws Exception {
         //1.导入jar包
-        Properties properties = new Properties();
+        Properties properties = new Properties(); //加载配置文件
         InputStream is = DruidDemo.class.getClassLoader().getResourceAsStream("druid.properties");
         properties.load(is);
+        //创建连接池
         DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
 
         Connection connection = dataSource.getConnection();
