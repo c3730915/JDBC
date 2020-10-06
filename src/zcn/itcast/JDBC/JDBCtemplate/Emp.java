58c78a5a16b9b50c935f28d6fac972c835bec986
c3730915 <1023700418@qq.com>
2020-10-06 15:16:19 +0800

    Emp类，为封装提供具体对象

---
+++ src/zcn/itcast/JDBC/JDBCtemplate/Emp.java
@@ -0,0 +1,92 @@
+package zcn.itcast.JDBC.JDBCtemplate;
+import java.util.Date;
+
+public class Emp {
+    private Integer id;
+    private String ename;
+    private Integer job_id;
+    private Integer mgr;
+    private Date joindate;
+    private Double salary;
+    private Double bouns;
+    private Double dept_id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public Integer getJob_id() {
+        return job_id;
+    }
+
+    public void setJob_id(Integer job_id) {
+        this.job_id = job_id;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public Date getJoindate() {
+        return joindate;
+    }
+
+    public void setJoindate(Date joindate) {
+        this.joindate = joindate;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBouns() {
+        return bouns;
+    }
+
+    public void setBouns(Double bouns) {
+        this.bouns = bouns;
+    }
+
+    public Double getDept_id() {
+        return dept_id;
+    }
+
+    public void setDept_id(Double dept_id) {
+        this.dept_id = dept_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "id=" + id +
+                ", ename='" + ename + '\'' +
+                ", job_id=" + job_id +
+                ", mgr=" + mgr +
+                ", joindate=" + joindate +
+                ", salary=" + salary +
+                ", bouns=" + bouns +
+                ", dept_id=" + dept_id +
+                '}';
+    }
+}
+
